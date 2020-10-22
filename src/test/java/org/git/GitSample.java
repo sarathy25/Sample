2c8a71613225a4f819d4ed3cca9e0bbacbba70f8
sarathy2511 <sarathy2511@gmail.com>
2020-10-22 14:53:46 +0530

    task is completed

---
+++ src/test/java/org/git/GitSample.java
@@ -0,0 +1,15 @@
+package org.git;
+
+public class GitSample {
+public static void main(String[] args) {
+	
+
+	String  name  = "asjfasjf" ;
+	if (name == "askfk") {
+		System.out.println("wel");
+	}
+	else {
+		System.out.println("invalid");
+	}
+	}
+}
